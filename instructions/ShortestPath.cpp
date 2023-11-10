f7e1aba28734c74d1b3cf4c8c9d164abcd1fe6eb
William Feng <wsfeng714@gmail.com>
2023-11-10 10:28:24 -0500

    Updated ShortestPath - many errors still need to be fixed

--- instructions/ShortestPath.cpp
+++ instructions/ShortestPath.cpp
@@ -5,13 +5,17 @@ using namespace std;
 /*
 TO-DO:
 --------------
+- CHANGE N TO 9, MARK ALL BORDER EDGES AS BAD
 - Implement custom measurements between zone 
 - Add # of turns as a factor for ShortestPath 
 
 */
 
 const int N = 7;
-const int GATES = 6;
+
+const int dx[] = {1, 0, -1, 0};
+const int dy[] = {0, -1, 0, 1};
+// const char dir[] = {'R', 'D', 'L', 'U'};
 
 int main() {
     freopen("input.in", "r", stdin);
@@ -42,23 +46,77 @@ int main() {
     }
     int n_wood; 
     cin >> n_wood;
-    vector<pair<pair<int, int>, char>> wood_blocks(n_wood);
-    for(auto pp : wood_blocks) {
-        cin >> p.first >> p.second.first >> p.second.second;
-    }
+    bool wood_blocks[N][N];
+    for(int i = 0; i < n_wood; i++) {
+        pair<int, int> p;
+        cin >> p.first >> p.second;
+        char c; cin >> c;
+        if(c == 'h') {
+            wood_blocks[p.first][p.second] = true;
+            if(p.first + 1 < N)
+                wood_blocks[p.first + 1][p.second] = true;
+            if(p.first - 1 >= 0)
+                wood_blocks[p.first - 1][p.second] = true;
+        }
+        else {
+            wood_blocks[p.first][p.second] = true;
+            if(p.second + 1 < N)
+                wood_blocks[p.first][p.second + 1] = true;
+            if(p.second - 1 >= 0)
+                wood_blocks[p.first][p.second - 1] = true;
+        }
+    }    
 
     // Graph Traversal
-    bool vis[N][N][1 << GATES];
-    memset(vis, false, sizeof(vis));
+    
+    // bool vis[N][N][1 << n_bonus];
+    // memset(vis, false, sizeof(vis));
+    
     auto valid_coordinate = [&](pair<int, int> coord) -> bool {
-        return coord.first >= 0 && coord.first < N && coord.second >= 0 && coord.second < N;
+        return coord.first >= 0 && coord.first < N && coord.second >= 0 && coord.second < N && !wood_blocks[coord.first][coord.second];
     };  
+    
     // Memory doesn't matter + Low Bounds, so I'm creating a huge multi-dimensional array to store paths
-    vector<pair<int, int>> minimum_path[N][N][1 << GATES]; 
+    
+    vector<pair<int, int>> minimum_path[N][N][1 << n_bonus]; 
+    
     // No cycles present, so brute-force BFS should work here
-    queue<pair<pair<int, int>, int> q;
+    
+    queue<pair<pair<int, int>, int>> q;
     q.push(make_pair(make_pair(start.first, start.second), 0));
     while(q.size()) {
-        
+        pair<int, int> coord = q.front().first;
+        int gates_visited = q.front().second;
+        q.pop();
+        for(int i = 0; i < 4; i++) {
+            pair<int, int> ncoord = make_pair(coord.first + dx[i], coord.second + dy[i]);
+            if(valid_coordinate(ncoord)) {
+                int ngates_visited = gates_visited;
+                if(bonus_zones.count(ncoord)) {
+                    ngates_visited |= (1 << bonus_zones[ncoord]);
+                }
+                if(minimum_path[ncoord.first][ncoord.second][ngates_visited].size() == 0) {
+                    if(valid_coordinate(coord)) {
+                        for(pair<int, int> p : minimum_path[coord.first][coord.second][gates_visited]) {
+                            minimum_path[ncoord.first][ncoord.second][ngates_visited].push_back(p);
+                        }
+                    }
+                    else {
+                        minimum_path[ncoord.first][ncoord.second][ngates_visited].push_back(coord);
+                    }
+                    minimum_path[ncoord.first][ncoord.second][ngates_visited].push_back(ncoord);
+                    q.push(make_pair(ncoord, ngates_visited));
+                }
+            } 
+        }
     }
+
+    // Found shortest path to target point, going through bonus zones
+    // Conversion to cardinal directions
+
+    // Check if there exists a path containing all bonus zones (wood blocks may block off gate zones)
+    for(int i = 0; i < (1 << n_bonus); i++){
+
+    }
+
 }
